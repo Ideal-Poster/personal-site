73832f99fb994f1b846656c77fc639cfde82d673
Malcolm Gourdine <malcolmgourdine@Malcolms-MBP.fios-router.home>
2020-03-13 22:19:21 -0400

    scroll fade

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,24 +3,44 @@ import './Home.css';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import { Row } from 'react-bootstrap';
+import anime from 'animejs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLongArrowAltDown } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faLongArrowAltDown)
+
 
 class Home extends React.Component {
+
+	constructor() {
+		super();
+		window.addEventListener('scroll', () => {
+			const a = (document.documentElement.scrollHeight - window.pageYOffset) -  window.innerHeight;
+			const b = window.innerHeight;
+			anime({
+				targets: '#splashpage-content',
+				opacity: a/b,
+				duration: 0.1
+			})
+		});
+	}
+
 	render() {
 		return(
 			<div>
 				<div id="splash-container">
 					<Container fluid={true}>
-						<p id="number">555&!</p>
-						<div id="homepage-text">
+						<div id="splashpage-content">
 							<Row>
 								<Col lg={10}>
-									<h1 id="splash-text"><span id="business-title">In the Streets Industries</span><br/> is creative design and technology by Malcolm Gourdine.</h1>
+									<h1 style={{zIndex: '100'}} id="splash-text"><span id="business-title">In the Streets Industries</span><br/> is creative design and technology by Malcolm Gourdine.</h1>
 								</Col>
 							</Row>
 
 							<Row>
 								<Col sm={{ offset: 2, span: 8 }}>
-									<h2 style={{textAlign: 'center', paddingBottom: '60px'}}>Scroll down to see some of my works</h2>
+									<h2 style={{textAlign: 'center', paddingBottom: '60px', fontFamily: 'Faune Italic'}}><FontAwesomeIcon style={{fontSize: '28px'}} icon="long-arrow-alt-down"/> Scroll down to see some of my works <FontAwesomeIcon style={{fontSize: '28px'}} icon="long-arrow-alt-down"/></h2>
 								</Col>
 							</Row>
 							<Row>
